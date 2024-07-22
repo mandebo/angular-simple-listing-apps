2e97463d36711fa7569c15c7f762783e88dc6719
Muhammad Aiman <aiman@agmo.group>
2024-07-22 16:50:50 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent,RouterModule],
+  styleUrl: './app.component.css',
+  template: `
+      <main>
+      <a [routerLink]="['/']">
+          <header class="brand-name">
+           <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+          </header>
+      </a>
+
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `
 })
 export class AppComponent {
-  title = 'angular-tutorial-1';
+  title = 'homes';
 }
